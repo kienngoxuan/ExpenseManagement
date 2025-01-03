097453fa3592a44cf4f5e81f781e6b1567414f19
Kien Ngo Xuan <ngoxuankien2005@gmail.com>
2025-01-03 08:57:17 +0700

    Old_user, 29/2 must be leap year (in time receive function)

--- customer.py
+++ customer.py
@@ -5,9 +5,10 @@ class User:
     receive_categories = ["Salary", "Investment", "Gift", "Business", "Allowance", "Bonus", "Loan", "Savings", "Pension", "Other"]
     users = []
 
-    def __init__(self, username, is_new_user):
+    def __init__(self, username, is_new_user, is_old_user):
         self.username = username
         self.is_new_user = is_new_user
+        self.is_old_user = is_old_user
         self.current_money = 0
         self.spend_money_day = []
         self.receive_money_day = []
@@ -15,8 +16,10 @@ class User:
     def welcome_user(self):
         if self.is_new_user:
             print(f"Welcome, {self.username}!")
-        else:
+        elif self.is_old_user:
             print(f"Welcome back, {self.username}!")
+        else:
+            print("Please choose wisely! (Y/N)")
 
     def set_current_money(self):
         if self.is_new_user:
@@ -56,21 +59,26 @@ class User:
 
     def time_receive_money(self):
         while True:
-            time_receive_choice = input("When do you receive this money? Today or another day ? (T/A) \n").strip().lower()
+            time_receive_choice = input("When do you receive this money? Today or another day? (T/A) ").strip().lower()
             if time_receive_choice == "t":
                 receive_today = datetime.now().strftime('%Y/%m/%d')
                 self.receive_money_day.append(receive_today)
                 break
             elif time_receive_choice == "a":
                 try:
-                    string_receive_anotherday = input("Enter the day you receive the money (DD/MM/YYYY): \n")
+                    string_receive_anotherday = input("Enter the day you receive the money (DD/MM/YYYY): ")
                     receive_anotherday = datetime.strptime(string_receive_anotherday, '%d/%m/%Y')
+                    if receive_anotherday.month == 2 and receive_anotherday.day == 29:
+                        if receive_anotherday.year % 4 != 0 or (receive_anotherday.year % 100 == 0 and receive_anotherday.year % 400 != 0):
+                            raise ValueError("February 29 is only valid in leap years.")
                     self.receive_money_day.append(receive_anotherday.strftime('%Y/%m/%d'))
                     break
-                except ValueError:
-                    print("Invalid date format. Please enter the date as DD/MM/YYYY.")
+                except ValueError as e:
+                    print(f"Invalid date! {str(e)}")
+                    print("Ensure the format is DD/MM/YYYY and that February 29 is in a leap year.")
             else:
                 print("Invalid choice. Please enter 'T' for today or 'A' for another day.\n")
+
 
 
     def spend_money(self):
